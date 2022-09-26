41250bc9579e51beffdb5e3bee8a1dd968edb1e1
Felix Pacheco <devfex.19@gmail.com>
2022-09-26 15:44:36 -0400

    Add new color to theme and set default font-family

--- src/globalStyles.ts
+++ src/globalStyles.ts
@@ -2,19 +2,17 @@ import { createGlobalStyle } from 'styled-components';
 
 export const globalTheme = {
     azure: '#1797ff',
+    brownishGray: '#6b6b6b',
 };
 
 const GlobalStyle = createGlobalStyle`
     * {
         box-sizing: border-box;
+        font-family: 'Roboto';
     }
     body {
         margin: 0;
         padding: 0;
-    }
-
-    code {
-        font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace;
     }
 `;
 
