2721fd963cd0e17815a3443d8e15228fe6ff186d
Felix Pacheco <devfex.19@gmail.com>
2022-09-26 15:44:51 -0400

    Start to implement FavoriteButton

---
+++ src/components/FavoriteButton.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import heartIcon from 'src/assets/heart-icon.svg';
+import heartFilledIcon from 'src/assets/heart-filled-icon.svg';
+import styled from 'styled-components';
+
+const LikeButton = styled.button`
+    background-color: transparent;
+    border: none;
+    width: 100%;
+    height: 100%;
+`;
+
+const FavoriteButton = () => {
+    const [isLiked, setIsLiked] = useState(false);
+
+    const toggleLike = () => setIsLiked(!isLiked);
+
+    return (
+        <LikeButton type="button" onClick={toggleLike}>
+            <img src={isLiked ? heartFilledIcon : heartIcon} alt="like icon" />
+        </LikeButton>
+    );
+};
+
+export default FavoriteButton;
