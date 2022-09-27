7e10444aedff2c44790311ff7c12c4bf9b6c5c2f
Felix Pacheco <devfex.19@gmail.com>
2022-09-27 11:42:57 -0400

    Refactor layout of App

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
 import styled, { ThemeProvider } from 'styled-components';
 import Header from './components/Header';
 import MainFilter from './components/MainFilter';
-import SelectFramework from './components/SelectFramework';
+import News from './components/News';
+import FiltersContextProvider from './contexts/FiltersContext';
 import { globalTheme } from './globalStyles';
 
-const ContainerApp = styled.main`
-    max-width: 1400px;
+const Main = styled.main`
+    max-width: 71.25rem;
     margin: auto;
+
+    > .main-filter-position {
+        text-align: center;
+        margin-top: 4.375rem;
+        margin-bottom: 3.938rem;
+    }
 `;
 
-function App() {
-    return (
+const App = () => (
+    <FiltersContextProvider>
         <ThemeProvider theme={globalTheme}>
-            <ContainerApp>
-                <Header />
-                <MainFilter />
-                <SelectFramework />
-            </ContainerApp>
+            <Header />
+            <Main>
+                <div className="main-filter-position">
+                    <MainFilter />
+                </div>
+                <News />
+            </Main>
         </ThemeProvider>
-    );
-}
+    </FiltersContextProvider>
+);
 
 export default App;
