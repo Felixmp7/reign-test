b769943bb69a985723619868c71e2e4bd218cb43
Felix Pacheco <devfex.19@gmail.com>
2022-09-27 11:48:42 -0400

    Move some files to News folder

--- src/components/News/index.tsx
+++ src/components/News/index.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import Card from '../Card';
+import Card from './Card';
 import SelectFramework from '../SelectFramework';
 import useNews from './useNews';
 
