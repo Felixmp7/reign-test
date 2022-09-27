ce502aeac8c9b32a82351cf5de0a7a1ed78c7fa5
Felix Pacheco <devfex.19@gmail.com>
2022-09-27 11:46:02 -0400

    Implement hook useFramework

---
+++ src/components/SelectFramework/useFramework.ts
@@ -0,0 +1,56 @@
+import { useEffect, useState } from 'react';
+import angularIcon from 'assets/angular-icon.svg';
+import reactIcon from 'assets/react-icon.svg';
+import vueIcon from 'assets/vue-icon.svg';
+import useFiltersContext from 'hooks/useFiltersContext';
+import { eQuery } from 'services/api/interfaces';
+
+export type FrameworkOption = {
+    label: string,
+    value: eQuery,
+    icon: string
+}
+
+const frameworks: FrameworkOption[] = [
+    { label: 'React', value: eQuery.react, icon: reactIcon },
+    { label: 'Angular', value: eQuery.angular, icon: angularIcon },
+    { label: 'Vuejs', value: eQuery.vue, icon: vueIcon },
+];
+
+const useFramework = () => {
+    const { filtersState, setFiltersState } = useFiltersContext();
+    const [isOpen, setIsOpen] = useState(false);
+    const [framework, setFramework] = useState<FrameworkOption | null>(null);
+
+    const toggleIsOpen = () => setIsOpen(!isOpen);
+
+    const handleGetCurrentFramework = (frameworkSetted: string | null) => {
+        if (frameworkSetted) {
+            const found = frameworks.find(({ value }) => value === frameworkSetted)!;
+            setFramework(found);
+        }
+    };
+
+    const onOptionClicked = (option: FrameworkOption) => {
+        if (option !== framework) {
+            setFramework(option);
+            setFiltersState({ ...filtersState, currentFramework: option.value });
+        }
+        setIsOpen(false);
+    };
+
+    useEffect(() => {
+        const frameworkSetted = filtersState.currentFramework;
+        handleGetCurrentFramework(frameworkSetted);
+    }, [filtersState.currentFramework]);
+
+    return {
+        isOpen,
+        frameworks,
+        framework,
+        onOptionClicked,
+        toggleIsOpen,
+    };
+};
+
+export default useFramework;
