b63ede128214099631da0d9cc344cb11d964ecb7
Felix Pacheco <devfex.19@gmail.com>
2022-09-28 12:10:02 -0400

    Add logic to handle likes and dislikes

--- src/components/News/FavoriteButton.tsx
+++ src/components/News/FavoriteButton.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import heartIcon from 'assets/heart-icon.svg';
 import heartFilledIcon from 'assets/heart-filled-icon.svg';
 import styled from 'styled-components';
+import { getStoriesLiked, saveStoriesLiked } from 'services/storage';
 
 const LikeButton = styled.button`
     background-color: transparent;
@@ -10,10 +11,23 @@ const LikeButton = styled.button`
     height: 100%;
 `;
 
-const FavoriteButton = () => {
-    const [isLiked, setIsLiked] = useState(false);
+type Props = {
+    objectId: string,
+    isAlreadyLiked?: boolean,
+}
 
-    const toggleLike = () => setIsLiked(!isLiked);
+const FavoriteButton = ({ objectId, isAlreadyLiked }: Props) => {
+    const [isLiked, setIsLiked] = useState(isAlreadyLiked);
+
+    const toggleLike = () => {
+        const currentStoriesLiked = getStoriesLiked() || [];
+        if (isLiked) {
+            saveStoriesLiked(currentStoriesLiked.filter((id) => id !== objectId));
+        } else {
+            saveStoriesLiked([...currentStoriesLiked, objectId]);
+        }
+        setIsLiked(!isLiked);
+    };
 
     return (
         <LikeButton type="button" onClick={toggleLike}>
