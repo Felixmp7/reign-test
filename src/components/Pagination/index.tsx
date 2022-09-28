f9ad9287683359ee51349ac1bb2ad69ae5032dea
Felix Pacheco <devfex.19@gmail.com>
2022-09-28 16:56:18 -0400

    Implement pagination

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,20 +1,64 @@
+import ChevronShape, { eChevronMode } from 'components/utils/ChevronShape';
 import styled from 'styled-components';
+import usePagination from './usePagination';
 
-const PaginationNavButton = styled.button`
+const ContainerPagination = styled.div<{ totalColumns: number }>`
+    display: inline-grid;
+    gap: .5rem;
+    grid-template-columns: ${({ totalColumns }) => `repeat(calc(2 + ${totalColumns}), minmax(0, 1fr))`};
+`;
+
+type SPaginationButtonProps = {
+    isActive?: boolean,
+}
+
+const PaginationButton = styled.button<SPaginationButtonProps>`
+    cursor: pointer;
     width: 2rem;
     height: 2rem;
     padding: 0.563rem;
     border-radius: 6px;
     border: solid 1px #d9d9d9;
-    background-color: #fff;
+    background-color: ${({ isActive, theme }) => (isActive ? theme.azure : '#fff')};
+    color: ${({ isActive }) => (isActive ? '#fff' : '#000000a5')};
+    display: inline-grid;
+    place-content: center;
+    line-height: 0;
+
+    &:hover {
+        background-color: ${({ isActive }) => (isActive ? '' : '#f2f2f2')};
+    }
 `;
 
-const Pagination = () => (
-    <div>
-        <PaginationNavButton type="button">
-            1
-        </PaginationNavButton>
-    </div>
-);
+const Pagination = () => {
+    const {
+        page,
+        currentListOfPages,
+        handleSetPage,
+        handleNextPage,
+        handlePrevPage,
+    } = usePagination();
+
+    return (
+        <ContainerPagination totalColumns={currentListOfPages.length}>
+            <PaginationButton type="button" onClick={handlePrevPage}>
+                <ChevronShape mode={eChevronMode.left} />
+            </PaginationButton>
+            {currentListOfPages.map((pageItem) => (
+                <PaginationButton
+                    key={pageItem}
+                    type="button"
+                    isActive={pageItem === page}
+                    onClick={() => handleSetPage(pageItem)}
+                >
+                    {pageItem}
+                </PaginationButton>
+            ))}
+            <PaginationButton type="button" onClick={handleNextPage}>
+                <ChevronShape mode={eChevronMode.right} />
+            </PaginationButton>
+        </ContainerPagination>
+    );
+};
 
 export default Pagination;
