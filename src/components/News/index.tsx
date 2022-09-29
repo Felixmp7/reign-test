4a9c6678db9e53a6abc5e7fea4fe0202ad58c329
Felix Pacheco <devfex.19@gmail.com>
2022-09-29 15:18:37 -0400

    Refactor News

--- src/components/News/index.tsx
+++ src/components/News/index.tsx
@@ -1,57 +1,68 @@
 import styled from 'styled-components';
 import useFilters from 'hooks/useFilters';
 import { getStoriesLiked } from 'services/storage';
+import { breakPoints } from 'globalStyles';
 import Card from './Card';
 import SelectFramework from '../SelectFramework';
 import useNews from './useNews';
+import { NoResults, Skeleton } from './helpers';
 
-const NewsGrid = styled.ul`
-    margin-top: 2.375rem;
-    display: grid;
-    grid-template-columns: repeat(2, minmax(0, 1fr));
-    padding: 0;
-    padding-right: .5rem;
-    gap: 2.5rem;
+const NewsGrid = styled.section`
     height: 400px;
-    overflow-y: auto;
+    margin-top: 2.375rem;
+
+    > ul {
+        display: grid;
+        grid-template-columns: repeat(2, minmax(0, 1fr));
+        padding: 0;
+        padding-right: .5rem;
+        gap: 2.5rem;
+        height: 100%;
+        overflow-y: auto;
+        align-content: start;
+
+        @media (max-width: ${breakPoints.tablet}) {
+            grid-template-columns: repeat(1, minmax(0, 1fr));
+        }
+    }
 `;
 
 const News = () => {
     const { filtersState: { isFilteredByFavs } } = useFilters();
-    const currentStoriesLiked = getStoriesLiked() || [];
-
     const { news, isLoading } = useNews();
+    const currentStoriesLiked = getStoriesLiked() || [];
 
     const newsFiltered = isFilteredByFavs
         ? news?.hits.filter(({ objectID }) => currentStoriesLiked.includes(objectID))
         : news?.hits;
 
     return (
-        <section>
+        <NewsGrid>
             {!isFilteredByFavs && <SelectFramework />}
             {(() => {
                 if (isLoading) {
-                    return 'Loading...';
+                    return <Skeleton />;
                 }
                 if (newsFiltered && newsFiltered.length > 0) {
                     return (
-                        <NewsGrid>
+                        <ul>
                             {newsFiltered.map((hit) => (
-                                <li style={{ listStyle: 'none' }} key={hit.objectID}>
+                                <li key={hit.objectID}>
                                     <Card
                                         cardTitle={hit.story_title}
                                         createdAt={hit.created_at}
                                         author={hit.author}
                                         objectId={hit.objectID}
+                                        storyLink={hit.story_url}
                                     />
                                 </li>
                             ))}
-                        </NewsGrid>
+                        </ul>
                     );
                 }
-                return <>No Favs added</>;
+                return <NoResults noResultsMessage={isFilteredByFavs ? 'No favs saved' : 'No results'} />;
             })()}
-        </section>
+        </NewsGrid>
     );
 };
 
